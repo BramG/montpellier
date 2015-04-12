737a0cd030e532b6ded86d8685af7e68839c51cb
Bram Goffings <bramgoffings@gmail.com>
2015-04-12 10:04:42 +0200

    Added settings form to DsField

--- montpellier/src/Plugin/DsField/MontpellierTitle.php
+++ montpellier/src/Plugin/DsField/MontpellierTitle.php
@@ -7,6 +7,7 @@
 namespace Drupal\montpellier\Plugin\DsField;
 
 use Drupal\ds\Plugin\DsField\DsFieldBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin that prefixes the title with "Montpellier"
@@ -23,13 +24,54 @@ class MontpellierTitle extends DsFieldBase {
    * {@inheritdoc}
    */
   public function build() {
+    $config = $this->getConfiguration();
+
     // Fetch the entity
     $node = $this->entity();
 
     // Always return a render array!
     return array(
-        '#markup' => $node->title->value,
-        '#prefix' => 'Montpellier ',
+      '#markup' => $node->title->value,
+      '#prefix' => $config['prefix'] . ' ',
     );
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm($form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+
+    $settings['prefix'] = array(
+      '#type' => 'textfield',
+      '#title' => 'Prefix',
+      '#default_value' => $config['prefix'],
+    );
+
+    return $settings;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary($settings) {
+    $config = $this->getConfiguration();
+
+    $summary = array();
+    if (!empty($config['prefix'])) {
+      $summary[] = 'Prefix: ' . $config['prefix'];
+    }
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array(
+      'prefix' => 'Montpellier',
+    );
+  }
+
 }
